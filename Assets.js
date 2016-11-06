34887bb828350ed76c4a163c830469b167a5f730
zacharye08 <zjervin@gmail.com>
2016-11-06 12:26:18 -0500

    Update Assets.js

--- Assets.js
+++ Assets.js
@@ -60,8 +60,10 @@ function AssetsFramework(){
 		for (var i = 0; i < iloadq.length; i++)
 			iloadq[i].img.src = iloadq[i].source;
 			var next = doneLoading.bind(this);
-		var interval = setTimeout(function(){
-			if (isloaded())next.call();},10);}
+		if (isloaded()) next.call();
+		else setTimeout(startload,15);
+		//var interval = setTimeout(function(){
+		//	if (isloaded())next.call();},10);}
 	function doneLoading(){
 		if (failed == 0) console.log("Asset loading complete. All files loaded successfully.");
 		else if (failed == 1) console.log("Asset loading complete. 1 file failed to load.")
